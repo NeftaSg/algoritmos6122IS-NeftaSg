56e6fe50af9ac0924c5d506e9f06663e8eb48db2
NeftaSg <114535278+NeftaSg@users.noreply.github.com>
2024-11-16 00:57:47 -0600

    PW pt3

---
+++ Programacion Web pt3/ProyectoPW/JS/app.js
@@ -0,0 +1,11 @@
+var s1 = document.getElementById('s1');
+var s2 = document.getElementById('s2');
+
+function mostrars2() {
+    s1.style.display = 'none';
+    s2.style.display = 'block';
+}
+function mostrars1(){
+    s1.style.display = 'block';
+    s2.style.display = 'none';
+}
